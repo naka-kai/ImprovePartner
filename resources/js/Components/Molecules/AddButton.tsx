e309291bf2c332f69c3326d96698417c2b81f540
kai <sea.k.naka@gmail.com>
2024-05-25 11:03:19 +0900

    プロジェクトを追加を左揃え、文字を小さく

--- resources/js/Components/Molecules/AddButton.tsx
+++ resources/js/Components/Molecules/AddButton.tsx
@@ -9,7 +9,7 @@ const AddButton: React.FC<Props> = ({ data }) => {
     return (
         <button className="my-3 text-gray-500 flex items-center justify-center">
             <span>
-                <AddOutlinedIcon sx={{ verticalAlign: '-6px' }} />
+                <AddOutlinedIcon sx={{ verticalAlign: '-3.5px' }} />
             </span>
             <span>{data}を追加</span>
         </button>

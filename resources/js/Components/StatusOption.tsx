069d066f367194730d5dd5218ee6a118e253fa03
kai <sea.k.naka@gmail.com>
2024-05-25 09:30:48 +0900

    プロジェクトタイトル表示

--- resources/js/Components/StatusOption.tsx
+++ resources/js/Components/StatusOption.tsx
@@ -2,6 +2,7 @@ import { StatusOptions } from '@/consts/IndexConsts'
 import FormControlLabel from '@mui/material/FormControlLabel'
 import Checkbox from '@mui/material/Checkbox'
 import React, { useState } from 'react'
+import { Typography } from '@mui/material'
 
 const StatusOption: React.FC = () => {
     const [selectedStatusIds, setSelectedStatusIds] = useState<number[]>([1, 2]) // 選択中のステータスID
@@ -34,7 +35,7 @@ const StatusOption: React.FC = () => {
 
     return (
         <div className="flex flex-col items-center">
-            <p className="mb-2 text-gray-500">ステータス</p>
+            <p className="mb-2 text-gray-500 text-sm">ステータス</p>
             <div>
                 {Object.values(StatusOptions).map((status) => (
                     <FormControlLabel
@@ -49,7 +50,11 @@ const StatusOption: React.FC = () => {
                                 }}
                             />
                         }
-                        label={toStringStatus(status)}
+                        label={
+                            <Typography sx={{ fontSize: 13 }}>
+                                {toStringStatus(status)}
+                            </Typography>
+                        }
                         className={
                             selectedStatusIds.includes(status)
                                 ? 'bg-sky-300 py-1 px-4 rounded-sm border-sky-300 border'
